0f9920591e8aa45b4504c2c0fb9a42295a335e03
ArnavPalkhiwala <palkhiwala.arnav@gmail.com>
2020-05-20 13:59:15 -0700

    First Commit

---
+++ TestJavaProject/FirstClass.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class FirstClass {
+public static void main(String[] args) {
+	System.out.println("Test One...");
+	System.out.println("It works");
+}
+}
---
+++ TestJavaProject/TestTwo.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class TestTwo {
+public static void main(String[] args) {
+	System.out.println("test 2...");
+}
+}
